1f77f051569b36563dbc4a3cecd1292bd509ee8a
hippiechick8 <57959762+hippiechick8@users.noreply.github.com>
2019-12-18 21:14:05 -0600

    functions for generating random characters for special, number, lowercase, and uppercase

--- script.js
+++ script.js
@@ -1,23 +1,52 @@
-// Assignment Code
-var generateBtn = document.querySelector("#generate");
 
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
 
-  copyBtn.removeAttribute("disabled");
-  copyBtn.focus();
+
+      // Assignment Code
+      var generateBtn = document.querySelector("#generate");
+
+
+/* functions to generate random special character, number, lowercase, 
+  and uppercase characters using assigned character numbers
+  https://www.ascii-code.com/ */
+
+function specialCharacter() {
+  return String.fromCharCode(Math.floor(Math.random() *10) + 33);
 }
 
-function copyToClipboard() {
-  // BONUS 
+function uppercaseCharacter() {
+  return String.fromCharCode(Math.floor(Math.random() *26) + 65);
 }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+function lowercaseCharacter() {
+  return String.fromCharCode(Math.floor(Math.random() *26) + 97);
+}
 
-// BONUS EVENT LISTENER
+function numericCharacter() {
+  return String.fromCharCode(Math.floor(Math.random() *10) + 48);
+}
+
+
+
+
+
+      // Write password to the #password input
+      function writePassword() {
+        var password = generatePassword();
+        var passwordText = document.querySelector("#password");
+
+        passwordText.value = password;
+
+        copyBtn.removeAttribute("disabled");
+        copyBtn.focus();
+      }
+
+      function copyToClipboard() {
+        // BONUS 
+      }
+
+      // Add event listener to generate button
+      generateBtn.addEventListener("click", writePassword);
+
+      // BONUS EVENT LISTENER
