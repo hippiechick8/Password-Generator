52c0c5c63da2d20dfb16a9d71032591e2b01f39f
hippiechick8 <57959762+hippiechick8@users.noreply.github.com>
2019-12-21 09:25:09 -0600

    button assignments

--- script.js
+++ script.js
@@ -3,4 +3,13 @@ var needSpecialChar;
 var needNumericChar;
 var needLowercaseChar;
 var needUppercaseChar;
+var startQuestions = true;
+
+// button assignments
+var yesBtn = document.querySelector("#yes");
+var noBtn = document.querySelector("#no");
+var generateBtn = document.querySelector("#generate");
+var copyBtn = document.querySelector("#copy");
+
+
 
